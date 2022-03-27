cce8b0d1fcc1c3cf762dd4f588dfcb0ddf18f263
AMOSAD0 <mossadahmed975@gmail.com>
2022-03-28 00:15:12 +0200

    M2

---
+++ lib/presentation/Module/Home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 10.h,
+          width: 80.w,
+          color: Colors.grey[200],
+        ),
+      ],      
+    );
+  }
+}
