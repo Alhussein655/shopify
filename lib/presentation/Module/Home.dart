4ad816e21d67569975a8d3326ca869c967174e2a
AMOSAD0 <mossadahmed975@gmail.com>
2022-03-28 00:56:16 +0200

    M3

--- lib/presentation/Module/Home.dart
+++ lib/presentation/Module/Home.dart
@@ -6,14 +6,80 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: 10.h,
-          width: 80.w,
-          color: Colors.grey[250],
-        ),
-      ],      
+    return Scaffold(
+      body: ListView(
+        children: [
+          //Search=======================
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 6.5.h,
+              width: 70.w,
+              decoration: BoxDecoration(
+               color: Colors.grey[300], 
+               borderRadius: BorderRadius.circular(30.sp),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(Icons.search,size: 25.sp,),
+                ],
+              ),
+            ),
+          ),
+          //=======================================//
+          //Text Categories=====================
+          Padding(
+            padding: const EdgeInsets.only(left: 16,top: 28),
+            child: Text('Categories',
+            style: TextStyle(
+              fontSize: 18.sp,
+              fontWeight: FontWeight.bold,
+            ),
+            ),
+          ),
+          //===================================//
+          //ICons========================
+          Padding(
+            padding: const EdgeInsets.only(left: 16,top: 19),
+            child: Container(
+              height: 18.h,
+              width: 30.w,
+              color: Colors.white,
+              child: ListView.builder(
+                itemCount: 5,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Container(
+                    height: 16.h,
+                    width: 23.w,
+                    color: Colors.white,
+                    child: Column(
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: Colors.black,
+                          radius: 35.sp,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text('data',
+                          style: TextStyle(
+                            fontSize: 12.sp,
+                            fontWeight: FontWeight.w400
+                          ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },),
+            ),
+          ),
+        ],      
+      ),
     );
   }
 }
